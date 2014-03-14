edf5e4043a95d5ecd66ee72b230a7d8c66d09702
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-14 23:32:39 +0800

    Generate helps

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-module.exports = function wechatHelp() {
-  return function wechatHelp(req, res, next) {
+module.exports = function createHelp(bot) {
+  function help(req, res, next) {
     var message = req.weixin;
+    var helps = bot.stack.map(function (mw) {
+      return mw.name + ":" + mw.help || 'ready';
+    });
     if (message) {
       return res.reply({
         type: 'text',
-        content: 'hello'
+        content: helps.join('\n')
       });
     } else {
       return next();
     }
   }
+  return help;
 };
