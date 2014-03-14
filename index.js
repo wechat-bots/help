a704fc25ed2b7b797d6cf4a38d44f025ac2aadf7
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-14 23:57:11 +0800

    Format help message

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 module.exports = function createHelp(bot) {
   function help(req, res, next) {
     var message = req.weixin;
-    var helps = bot.stack.map(function (mw) {
-      return mw.name + ":" + mw.help || 'ready';
+    var helps = [];
+    var i = 1;
+    bot.stack.forEach(function(mw) {
+      var handle = mw.handle;
+      if (handle.help) {
+        helps.push((i++) + ' - ' + handle.help);
+      }
     });
     if (message) {
       return res.reply({
