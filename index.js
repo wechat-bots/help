41874570516f4ff22b4655782ce022e79e18444f
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-16 12:12:57 +0800

    Add test case

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+var util = require('util');
+
 module.exports = function createHelp(bot) {
   function help(req, res, next) {
     var message = req.weixin;
     var helps = [];
-    var i = 1;
     bot.stack.forEach(function(mw) {
       var handle = mw.handle;
       if (handle.help) {
-        helps.push((i++) + ' - ' + handle.help);
+        helps.push(util.format('[%s] %s', handle.name, handle.help));
       }
     });
     if (message) {
@@ -18,5 +19,6 @@ module.exports = function createHelp(bot) {
       return next();
     }
   }
+  help.help = 'help message auto generated';
   return help;
 };
--- test/index.js
+++ test/index.js
@@ -1,8 +1,25 @@
 var expect = require('chai').expect,
     wechatHelp = require('..');
+var wechatBot = require('wechat-bot');
 
 describe('wechat-help', function() {
-  it('should export middleware creater', function() {
+  it('should export middleware creater', function () {
     expect(wechatHelp()).to.be.a('function');
+  });
+  it('should generate help message', function (done) {
+    var bot = wechatBot();
+    var help = wechatHelp(bot);
+    var req = {
+      weixin: {}
+    };
+    var res = {
+      reply: function (result) {
+        expect(result.content).to.include(help.help);
+        expect(result.content).to.include(help.name);
+        done();
+      }
+    };
+    bot.use(help);
+    bot(req, res);
   });
 });
